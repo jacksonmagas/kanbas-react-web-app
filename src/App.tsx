cebdc31ec153025f52da607ed9732afc2f9b3b7f
Jackson <jacksonmagas@gmail.com>
2024-09-17 17:15:44 -0400

    Initial Account screens added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Kanbas from './Kanbas';
 import Labs from './Labs';
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
@@ -7,8 +8,9 @@ function App() {
     <HashRouter>
       <div>
         <Routes>
-	  <Route path="/" element={<Navigate to="Labs" />} />
+	  <Route path="/" element={<Navigate to="Kanbas" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
     </HashRouter>
---
+++ src/Kanbas/Account/index.tsx
@@ -0,0 +1,14 @@
+import Signin from "./Signin";
+import Profile from "./Profile";
+import Signup from "./Signup";
+
+export default function Account() {
+  return (
+    <div id="wd-account-screen">
+      <h2>Account</h2>
+      <Signin />
+      <Profile />
+      <Signup />
+    </div>
+  );
+}
