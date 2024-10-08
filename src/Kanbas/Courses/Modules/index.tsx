5e90fb9129a0ef7f63e4a68d8c0d23d3802100b4
Jackson <jacksonmagas@gmail.com>
2024-10-08 14:00:12 -0400

    Add untracked files

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,5 +1,8 @@
 import ModulesControls from "./ModulesControls";
+import "../../styles.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BsGripVertical } from "react-icons/bs";
+import ModuleControlButtons from "./ModuleControlButtons";
 
 export default function Modules() {
   return (
@@ -7,7 +10,11 @@ export default function Modules() {
   <ModulesControls /><br /><br /><br /><br />
   <ul id="wd-modules" className="list-group rounded-0">
     <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-      <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
+      <div className="wd-title p-3 ps-2 bg-secondary">
+        <BsGripVertical className="me-2 fs-3" />
+        Week 1
+        <ModuleControlButtons />
+      </div>
       <ul className="wd-lessons list-group rounded-0">
         <li className="wd-lesson list-group-item p-3 ps-1">
           LEARNING OBJECTIVES </li>
