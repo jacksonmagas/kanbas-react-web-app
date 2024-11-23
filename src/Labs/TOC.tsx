e4af04f5ac6be053c5ae88849bd6efdf4416a547
Jackson <jacksonmagas@gmail.com>
2024-11-23 18:53:50 -0500

    Add github to TOC

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -45,7 +45,12 @@ export default function TOC() {
       </li>
       <li className="nav-item">
         <a id="wd-k" href="https://github.com/jacksonmagas/kanbas-react-web-app" className="nav-link">
-          My GitHub
+          Frontend Github
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-k" href="https://github.com/jacksonmagas/kanbas-node-server-app" className="nav-link">
+          Server Github
         </a>
       </li>
     </ul>
