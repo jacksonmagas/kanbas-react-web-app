5e49d3e5ac96f82a091c824dab98d19efa19e28b
Jackson <jacksonmagas@gmail.com>
2024-11-28 22:42:43 -0500

    Add question editor to main page for debug

--- src/Kanbas/Account/RoleShownContent.tsx
+++ src/Kanbas/Account/RoleShownContent.tsx
@@ -3,8 +3,8 @@ import { User } from "./reducer";
 
 export function StudentView({ children }: { children: any }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const { view } = useSelector((state: any) => state.viewReducer);
-  if (currentUser?.role === "STUDENT" || view === "STUDENT") {
+  //const { view } = useSelector((state: any) => state.viewReducer);
+  if (currentUser?.role === "STUDENT"/* || view === "STUDENT"*/) {
     return children;
   } else {
     return <div/>;
@@ -12,8 +12,8 @@ export function StudentView({ children }: { children: any }) {
 
 export function FacultyView({ children }: { children: any }) {
   const { currentUser } : { currentUser : User | undefined} = useSelector((state: any) => state.accountReducer);
-  const { view } : { view : string | undefined} = useSelector((state: any) => state.viewReducer);
-  if (currentUser?.role === "FACULTY" && view === "FACULTY") {
+  //const { view } : { view : string | undefined} = useSelector((state: any) => state.viewReducer);
+  if (currentUser?.role === "FACULTY"/* && view === "FACULTY"*/) {
     return children;
   } else {
     return <div/>;
